69969937a04a8c188e8aef0a7a24585156c1bf92
uhryniuk <duhry99@gmail.com>
2022-09-17 21:37:17 -0500

    Fixing stuff

--- Driver.java
+++ Driver.java
@@ -5,6 +5,6 @@ public class Driver{
     public static void main(String[] args){
         int a[] = {2,3,4,5,6,7};
         int sum = ArrayMath.sum(a);
-        System.out.println("Hello World! "+sum);
+        System.out.println("Hello Wold! "+sum);
     }
 }
